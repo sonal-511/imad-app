ed576a70d2d13dc804e3bf3577d5cbc66d47a968
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-08-14 01:10:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.sendfile(path.join(--dirname,'ui','article-two.html'));
+    res.sendfile(path.join(__dirname,'ui','article-two.html'));
 });
 app.get('/article-three',function(req,res){
       res.sendfile(path.join(__dirname,'ui','article-three.html'));
