7ce5ccf7deaf92cd65f09ef3f6c3360b77143396
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-08-15 01:29:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/ui/style.css', function(req,res){
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get("/ui/main.js",function(req,res){
+app.get('/ui/main.js',function(req,res){
   res.senFile(__dirname,"ui","main.js");  
 });
 
