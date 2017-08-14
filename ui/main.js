5c6f6731bde7f9d3e7839839b764e6d56cf1d88b
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-08-15 01:53:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 
 var element =document.getElementById('quk');
 element.innerHTML='new value here i come';
+var ih=document.getElementById('ijk');
+ih.onclick=function(){
+ih.style.marginLeft='100px';    
+};
