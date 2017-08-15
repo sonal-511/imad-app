a13bd9bbd88a82462366130b880564e276dbd939
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-08-15 12:10:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,3 +12,15 @@ ih.onclick=function(){
 var interval=setInterval(moveleft,100);
 };
 */
+var counter=0;
+var element=documnet.getElementById('counter');
+element.onClick=function(){
+ //make a request to counter endpoint
+ 
+ //captutre the response and store it in a variable
+ 
+ //render the variable in correct span
+ counter=counter+1;
+ var span=document.getElementById('count');
+ span.innerHTML=counter.toString();
+};
