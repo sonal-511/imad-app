98d8ef8d117facf990d1e22402bf91bc4e837077
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-08-15 12:47:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,19 +88,20 @@ app.get('/', function (req, res) {
 app.get('/plz', function (req, res) {
   res.send('hie');
 });
-var counter=0;
-app.get('/art', function( req, res){
-    counter=counter+1;
-    res.send(counter.toString());
-    
-    res.send("hello");
-});
+
 app.get('/:mety',function(req,res){
     var mety=req.params.mety;
     res.send(createtemplatedata(articles[mety]));
 });
 app.get('/ui/style.css', function(req,res){
     res.sendFile(path.join(__dirname,'ui','style.css'));
+});
+var counter=0;
+app.get('/art', function( req, res){
+    counter=counter+1;
+    res.send(counter.toString());
+    
+    res.send("hello");
 });
 
 
