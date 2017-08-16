9c0fb08a3d7dc98e19a5e10f437062b199f04b15
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-08-17 00:53:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/art', function( req, res){
     res.send("hello");
 });
 var names=[];
-app.get('submit-name/:name',function(req,res){
+app.get('/submit-name/:name',function(req,res){
  var name=req.params.name;
  names.push(name);
  res.send(JSON.stringfy(names));
