87ac3a6b12f55a2de3213b22892c800f428688b1
sonal-511 <30926024+sonal-511@users.noreply.github.com>
2017-08-17 01:12:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,20 +29,29 @@ element.onclick=function(){
  kick.send(null);
 };
 
-var namevarr=document.getElementById('name1');
-var name=namevarr.value;
+
 var submit=document.getElementById('submit');
 submit.onclick=function(){
- 
+ var namevarr=document.getElementById('name1');
+var name=namevarr.value;
  //make a request to the server and send the names
- 
- //capture a list of names an drender it
- var names=['name1','name2','name3','name4'];
+   var kick=new XMLHttpRequest();
+ //captutre the response and store it in a variable
+ if(kick.readychange===XMLHttpRequest.DONE && kick.status===200)
+ {
+    var names=kick.responseText;
+     names=JSON.parce(names);
  var list='';
  for(var i=0;i<names.length;i++){
  
     list=list+'<li>'+names[i]+'</li>'; 
+ //capture a list of names an drender it
  }
- var ul=document.getElementById('name_list');
- ul.innerHTML=list
+  var ul=document.getElementById('name_list');
+ ul.innerHTML=list;
+
+ }
+
+ res.open('GET', "http://sonalgupta115.imad.hasura-app.io/submit-name",true);
+ res.open(null);
 };
